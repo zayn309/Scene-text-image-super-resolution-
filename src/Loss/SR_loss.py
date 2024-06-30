8333e0637820cb39cf02d3848fb166dd62fbc004
zayn <102887305+zayn309@users.noreply.github.com>
2024-06-30 14:25:13 +0000

    using L1 norm instead of ch los

--- src/Loss/SR_loss.py
+++ src/Loss/SR_loss.py
@@ -1,15 +1,13 @@
 import torch
 import torch.nn as nn
 
-
 class CharbonnierLoss(nn.Module):
-    """Charbonnier Loss (L1)"""
+    """Pixel-wise L1 Loss"""
 
-    def __init__(self, eps=1e-3):
+    def __init__(self):
         super(CharbonnierLoss, self).__init__()
-        self.eps = eps
 
     def forward(self, x, y):
-        diff = x - y
-        loss = torch.mean(torch.sqrt(diff * diff + self.eps ** 2))
+        loss = torch.mean(torch.abs(x - y))
         return loss
+
